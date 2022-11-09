427ba0a1775000fba0361d81fdff499bf9d6f11f
tugbacanbaz <tugbacanbaz.pi@gmail.com>
2022-11-09 23:45:55 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1=31;
 
-// Write your code here
+const num2=2;
+
+const multiply=num1*num2;
+
+const random = Math.floor(Math.random() * 10);
+
+const num3 = 14;
+const num4 = 10;
+const mod = num3 % num4;
+
+const max = Math.max(1,2,20);
